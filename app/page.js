04a5cc09bf43d968250bf071ae5cbe52279ab0d5
gabriel note <gabriel.emil.note@gmail.com>
2026-05-03 15:46:57 +0200

    använder useState

--- app/page.js
+++ app/page.js
@@ -1,26 +1,32 @@
 "use client";
 
 import Test from "./components/test";
-import { io } from "socket.io-client";
+import ConnectSocket from "./components/connectsocket";
+import { useState } from "react";
 console.log("yesyesyes");
 
-const handleMessageButton = (e) => {
-  e.preventDefault();
-  console.log("we pressed the button and sent a message");
-}
-
-
-
 export default function Home() {
+  const [message, setMessage] = useState("");
+
+  const handleMessageButton = (e) => {
+    e.preventDefault();
+    ConnectSocket.emit("message", message);
+    console.log("we pressed the button and sent a message");
+  };
+
   return (
     <div>
       <h1>Battleship Websocket Demo</h1>
       <Test />
-      <form onSubmit={handleMessageButton} >
-        <input type="text" placeholder="Enter message" />
+      <form onSubmit={handleMessageButton}>
+        <input
+          type="text"
+          placeholder="Enter message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <button type="submit">Send</button>
       </form>
     </div>
-    
   );
 }
